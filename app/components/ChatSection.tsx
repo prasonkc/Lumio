8bd1615bc3f665be07e3712a8515df3c1f02af0d
Prason Kc <prasonkc07@gmail.com>
2025-10-09 21:06:46 +0545

    Minor client chat 1

--- app/components/ChatSection.tsx
+++ app/components/ChatSection.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatBubble from "./ChatBubble";
 import { Send } from "lucide-react";
 import io from "socket.io-client";
 
+// initialize socket server
+const socket = io("http://localhost:4000", {
+  transports: ["websocket", "polling"],
+});
+
 const ChatSection = () => {
+  const [msg, setMsg] = useState("");
   useEffect(() => {
-    // initialize socket server
-    const socket = io("http://localhost:4000", {
-      transports: ["websocket", "polling"],
-    });
     // Connect to server
     socket.on("connect", () => {
       console.log("connected", socket.id);
@@ -16,14 +18,19 @@ const ChatSection = () => {
 
     socket.on("new-message", (msg) => {
       console.log("New message:", msg);
+      setMsg(msg)
     });
   }, []);
+
+  const sendMessage = () => {
+      socket?.emit("send-message", msg);
+  };
 
   return (
     <div className="bg-gray-700 w-2/3 min-h-screen p-3 relative">
       <ChatBubble
         isSender={false}
-        text="Hello World"
+        text={msg}
         avatarUrl="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
       />
 
@@ -40,8 +47,13 @@ const ChatSection = () => {
             type="text"
             placeholder="Message..."
             className="flex-1 bg-transparent outline-none text-white placeholder-gray-300"
+            onChange={(e) => setMsg(e.target.value)}
+            value={msg}
           />
-          <button className="ml-2 text-blue-400 hover:text-blue-500 cursor-pointer">
+          <button
+            className="ml-2 text-blue-400 hover:text-blue-500 cursor-pointer"
+            onClick={sendMessage}
+          >
             <Send className="w-6 h-6" />
           </button>
         </div>
