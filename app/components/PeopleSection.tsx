8e1327e58222c6772d536a6c96d9faba1f62c362
Prason Kc <prasonkc07@gmail.com>
2025-10-15 08:36:28 +0545

    Frontend fetch for contacts

--- app/components/PeopleSection.tsx
+++ app/components/PeopleSection.tsx
@@ -1,12 +1,25 @@
+"use client"
 import React from "react";
 import { MoreVertical, Search, MessageCircle, Users } from "lucide-react";
 import Person from "./Person";
+import { useEffect, useState } from "react";
 
 interface PeopleProps {
   name: string;
 }
 
 const PeopleSection: React.FC<PeopleProps> = ({ name }) => {
+  const [contacts, setContacts] = useState([])
+ useEffect(() => {
+    const fetchContacts = async () => {
+      const res = await fetch("/api/contacts");
+      const data = await res.json();
+      setContacts(data);
+    };
+
+    fetchContacts();
+  }, []);
+  
   return (
     <div className="flex flex-col bg-gray-900 w-80 min-h-screen border-r border-gray-800">
       {/* Header */}
@@ -34,10 +47,9 @@ const PeopleSection: React.FC<PeopleProps> = ({ name }) => {
 
       {/* People List */}
       <div className="flex-1 overflow-y-auto px-3 py-2 scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-900">
-        <Person name="Alice" />
-        <Person name="Bob" />
-        <Person name="Charlie" />
-        <Person name={name} />
+        {contacts.map((person) => (
+          <Person name={person}/>
+        ))}
       </div>
     </div>
   );
