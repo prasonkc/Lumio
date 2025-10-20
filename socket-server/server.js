0c7805491f740e2905fe03f2a3e72fab36d4071a
Prason Kc <prasonkc07@gmail.com>
2025-10-20 10:41:13 +0545

    port change

--- socket-server/server.js
+++ socket-server/server.js
@@ -43,6 +43,7 @@ io.on("connection", (socket) => {
 });
 
 //start server once
-httpServer.listen(4000, () => {
+
+httpServer.listen(process.env.PORT || 4000, () => {
   console.log("Socket.IO server running on port 4000");
 });
